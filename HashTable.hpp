14581be78338efa3f0495482a41015ef20ae10f4
idnsaaife <164374042+idnsaaife@users.noreply.github.com>
2024-11-05 09:53:06 +0700

    Update HashTable.hpp

--- HashTable.hpp
+++ HashTable.hpp
@@ -1,16 +1,18 @@
+#pragma once
 #include "List.hpp"
-#include <iostream>
-
 
 class HashTable
 {
-    static constexpr size_t capacity = 191;
-    List *arr;
+    static constexpr size_t INIT_CAPACITY = 2;
+    static constexpr double MAX_LOAD_FACTOR = 0.75;
+    size_t capacity = INIT_CAPACITY;
     size_t curAmount = 0;
+    List *arr = nullptr;
+    void rehash();
 
 public:
     /*
-    Constructs a new HashTable object with a default capacity 191
+    Constructs a new HashTable object with a default capacity
      */
     HashTable();
     /*
@@ -20,38 +22,68 @@ public:
     /*
     Copy constructor
     Creates a deep copy of another HashTable object
-    b  The HashTable object to copy
+    b  is the HashTable object to copy
     */
     HashTable(const HashTable &b);
     /*
     Move constructor
     Creates new HashTable object by moving another HashTable object to this one
-    b The HashTable to move
+    b is the HashTable to move
     */
     HashTable(HashTable &&b);
-    /*swap two HashTable objects*/
+    /*
+    swap two HashTable objects
+    */
     void swap(HashTable &b);
     /*
     assigns values to the container
     */
     HashTable &operator=(const HashTable &b);
+    HashTable &operator=(HashTable &&b);
     /*
     clears the contents
     */
     void clear();
-    /*erases elements by key*/
+    /*
+    erases elements by key
+    returns true if element was in HashTable
+    returns false if element wasn't in HashTable
+    */
     bool erase(const Key &k);
-    /*inserts elements 
-    return true if element is new
-    return false if element with this key already added to HashTable or */
-    bool insert(const Key &k, const Value &v); 
+    /*inserts elements
+    returns true if element is new
+    returns false if element with this key already added to HashTable 
+    */
+    bool insert(const Key &k, const Value &v);
+    /*
+    checks if the container contains element with specific key 
+    */
     bool contains(const Key &k) const;
+    /*
+    returns a value by key, 
+    if container doesn't contain this value inserts the value created by default constructor and returns a reference to it 
+    */
     Value &operator[](const Key &k);
+    /*
+    returns a value by key, throws an exception(std::runtime_error) on failure
+    */
     Value &at(const Key &k);
     const Value &at(const Key &k) const;
+    /*
+    returns capacity
+    */
     size_t size() const;
-    size_t amount() const; 
-    bool empty() const; 
+    /*
+    returns number of added elements
+    */
+    size_t amount() const;
+    /*
+    checks whether the container is empty 
+    */
+    bool empty() const;
+    /*
+    compares the values in two HashTable objects
+    */
     friend bool operator==(const HashTable &a, const HashTable &b);
     friend bool operator!=(const HashTable &a, const HashTable &b);
 };
