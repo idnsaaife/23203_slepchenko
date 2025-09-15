5edef2c1b09716115d7a31b48fe15357a9ae5f07
idnsaaife <164374042+idnsaaife@users.noreply.github.com>
2025-09-15 13:10:03 +0700

    Delete javaLabs/MyLabs directory

--- javaLabs/MyLabs/src/myunit/RunRunRun.java
+++
@@ -1,9 +0,0 @@
-package myunit;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Retention;
-
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface RunRunRun {
-    String why() default "";
-    int chance() default 1;
-}
